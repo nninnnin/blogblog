029d2e6d2b995c46af2e2e28a4f100e0ab953d43
donggyulee <nninnnin7@gmail.com>
2025-05-24 02:23:44 +0900

    파사드 간격과 스케일링 조정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,16 +7,8 @@ import { useEffect, useMemo, useState } from "react";
 export default function Home() {
   const [date, setDate] = useState(new Date());
 
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     setDate(new Date());
-  //   }, 1000 * 10);
-
-  //   return () => clearInterval(interval);
-  // }, []);
-
   const aligns = useMemo(() => {
-    return fill(Array(200), null).map(() =>
+    return fill(Array(500), null).map(() =>
       sample(["text-right", "text-left", "text-center"])
     );
   }, []);
@@ -45,24 +37,42 @@ export default function Home() {
 
 const Time = ({ className, date }: { className: string; date: Date }) => (
   <div
-    className={clsx(className)}
+    className={clsx("scailing", className)}
     style={{
       letterSpacing: sample([
-        "-10px",
+        "-3vw",
+        "-1vw",
+        "-1em",
         "-8px",
         "-5px",
         "-2px",
         "1px",
-        "2px",
         "3px",
         "4px",
         "10px",
-        "100px",
+        "1em",
+        "3vw",
+        "5vw",
       ]),
-      fontSize: sample(["8px", "12px", "18px"]),
+      fontSize: sample([
+        "0.05vw",
+        "0.1vw",
+        "0.4vw",
+        "0.45vw",
+        "0.5vw",
+        "0.55vw",
+        "0.6vw",
+        "0.65vw",
+        "0.9vw",
+        "0.95vw",
+        "1vw",
+        "2vw",
+      ]),
       lineHeight: sample([
-        "-2em",
+        "-1vw",
         "-1em",
+        "-0.2em",
+        "-0.5em",
         "0.1em",
         "0.2em",
         "0.3em",
@@ -73,8 +83,7 @@ const Time = ({ className, date }: { className: string; date: Date }) => (
         "0.8em",
         "0.9em",
         "1em",
-        "1.2em",
-        "2em",
+        "1vw",
       ]),
     }}
     suppressHydrationWarning
