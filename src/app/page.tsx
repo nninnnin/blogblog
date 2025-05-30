d83551cc693c96294553cf09bdecb9afd8bff86d
donggyulee <nninnnin7@gmail.com>
2025-05-30 22:06:31 +0900

    Remove time based cache revalidation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@ import clsx from "clsx";
 
 import Facade from "@/app/components/facade";
 
-export const revalidate = 1; // seconds
-
 export default function Home() {
   return (
     <div className={clsx("h-full flex flex-col justify-center items-center")}>
