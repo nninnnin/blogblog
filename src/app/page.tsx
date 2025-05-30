069675c5d07167727ebf182bf829aacec34de678
donggyulee <nninnnin7@gmail.com>
2025-05-30 22:09:52 +0900

    Revalidate home page when it is mounted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
+"use client";
+
 import clsx from "clsx";
+import { useEffect } from "react";
 
 import Facade from "@/app/components/facade";
 
 export default function Home() {
+  useEffect(() => {
+    (async function () {
+      await fetch("/revalidate/api");
+    })();
+  }, []);
+
   return (
     <div className={clsx("h-full flex flex-col justify-center items-center")}>
       <Facade />
